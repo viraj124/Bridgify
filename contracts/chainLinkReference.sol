592aa3e91367f950a7709217b42533a97841411b
Viraz Malhotra <virajm72@gmail.com>
2020-07-15 16:13:45 +0530

    feature(Updated the contract to get both erc20-usd & usd-fiat rates in a single call)

--- contracts/chainLinkReference.sol
+++ contracts/chainLinkReference.sol
@@ -4,7 +4,9 @@ import "https://github.com/smartcontractkit/chainlink/evm-contracts/src/v0.6/int
 
 
 contract ReferenceConsumer {
-  AggregatorInterface internal ref;
+  AggregatorInterface internal erc20Ref;
+  AggregatorInterface internal fiatRef;
+
   
 modifier onlyOwner () {
     require(owner == msg.sender, "Only called by owner");
@@ -17,14 +19,15 @@ constructor() public {
     owner = msg.sender;
 }
 
-function setReferenceContract(address _aggregator)
+function setReferenceContract(address _erc20aggregator, address _fiataggregator)
   public
   onlyOwner()
 {
-  ref = AggregatorInterface(_aggregator);
+  erc20Ref = AggregatorInterface(_erc20aggregator);
+  fiatRef = AggregatorInterface(_fiataggregator);
 }
   // get exchange rate
-  function getLatestAnswer() public view returns (int256) {
-    return ref.latestAnswer();
+  function getLatestAnswer() public view returns (int256, int256) {
+    return (erc20Ref.latestAnswer(), fiatRef.latestAnswer()) ;
   }
 }
