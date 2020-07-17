880ef3957799cb83052cec918e328d66021b023d
Viraz Malhotra <virajm72@gmail.com>
2020-07-17 11:20:24 +0530

    Feature(Added SafeMath Library & Updated the logic to get the minting amount")

--- contracts/chainLinkReference.sol
+++ contracts/chainLinkReference.sol
@@ -2,8 +2,165 @@ pragma solidity ^0.6.0;
 
 import "https://github.com/smartcontractkit/chainlink/evm-contracts/src/v0.6/interfaces/AggregatorInterface.sol";
 
+/**
+ * @dev Wrappers over Solidity's arithmetic operations with added overflow
+ * checks.
+ *
+ * Arithmetic operations in Solidity wrap on overflow. This can easily result
+ * in bugs, because programmers usually assume that an overflow raises an
+ * error, which is the standard behavior in high level programming languages.
+ * `SafeMath` restores this intuition by reverting the transaction when an
+ * operation overflows.
+ *
+ * Using this library instead of the unchecked operations eliminates an entire
+ * class of bugs, so it's recommended to use it always.
+ */
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     *
+     * - Addition cannot overflow.
+     */
+    function add(int256 a, int256 b) internal pure returns (int256) {
+        int256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(int256 a, int256 b) internal pure returns (int256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(int256 a, int256 b, string memory errorMessage) internal pure returns (int256) {
+        require(b <= a, errorMessage);
+        int256 c = a - b;
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     *
+     * - Multiplication cannot overflow.
+     */
+    function mul(int256 a, int256 b) internal pure returns (int256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        int256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(int256 a, int256 b) internal pure returns (int256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(int256 a, int256 b, string memory errorMessage) internal pure returns (int256) {
+        require(b > 0, errorMessage);
+        int256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(int256 a, int256 b) internal pure returns (int256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts with custom message when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(int256 a, int256 b, string memory errorMessage) internal pure returns (int256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+}
 
 contract ReferenceConsumer {
+//   using SafeMath for uint;
+  using SafeMath for int256;
   AggregatorInterface internal erc20Ref;
   AggregatorInterface internal fiatRef;
 
@@ -26,8 +183,14 @@ function setReferenceContract(address _erc20aggregator, address _fiataggregator)
   erc20Ref = AggregatorInterface(_erc20aggregator);
   fiatRef = AggregatorInterface(_fiataggregator);
 }
-  // get exchange rate
-  function getLatestAnswer() public view returns (int256, int256) {
-    return (erc20Ref.latestAnswer(), fiatRef.latestAnswer()) ;
+  // mint erc20 afte calculating the exchange rate for dai/fiat
+  function getLatestAnswer(int256 _daiAmount) public view returns(int256, int256, int256)  {
+    // usd pairs are multiplied with 10^8 to account decimal places so converting the final result to wei
+    int256 usdExchangeAmount = erc20Ref.latestAnswer().mul(_daiAmount);
+    int256 fiatMintingAmount = usdExchangeAmount.div(fiatRef.latestAnswer());
+    return (fiatMintingAmount, erc20Ref.latestAnswer(), fiatRef.latestAnswer());
+    
+    // minting is left and conversion to wei
+//     return (erc20Ref.latestAnswer(), fiatRef.latestAnswer()) ;
   }
 }
